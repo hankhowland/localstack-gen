7ad2e19da8e54f1acdcf4c01e892e22240f899d0
Henry Myers <hankhowland@gmail.com>
2021-07-21 09:43:25 -0700

    improvements

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ console.log(data);
 
 function App() {
   const [service, setService] = useState('');
-  const [command, setCommand] = useState('s');
+  const [command, setCommand] = useState('');
   const [detailType, setDetailType] = useState('');
   const [commandText, setCommandText] = useState('');
   const [object, setObject] = useState('');
@@ -33,11 +33,17 @@ function App() {
 
   useEffect(() => {
     function makeCommand() {
+      if (command === '' || service === '') {
+        return '--no command yet--'
+      }
       const endpoint = data.services[service]?.endpoint
       const queueURL = data.services[service]?.queueURL
       console.log(object)
       console.log(JSON.stringify(object))
       if (command ==='send') {
+        if (detailType === '' || object === '') {
+          return '--no command yet--'
+        }
         const dataName = getDTInfo()?.detailField
         const str = `aws --endpoint-url=${endpoint} sqs send-message --queue-url ${queueURL} --message-body '{ "version": "0", "id": "a2a13500-9ad4-f384-new-new", "detail-type": ${detailType}, "source": "guzzle-dev", "account": "188291258129", "time": "2021-04-13T19:38:50Z", "region": "us-west-1", "resources": [], "detail": { "${dataName}": ${JSON.stringify(object)}} }'`
         return str;
@@ -48,11 +54,31 @@ function App() {
       }
     }
     setCommandText(makeCommand());
+    document.getElementById("copyButton").innerHTML = 'COPY'
   }, [command, service, detailType, object])
+
+  function CopyCommand() {
+    var textArea = document.createElement("textarea");
+    textArea.value = commandText;
+
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+
+    document.getElementById("copyButton").innerHTML = 'COPIED &#10003;'
+  }
 
   return (
     <div className="App">
-      <div style={{width: '50%', margin: 'auto'}}>
+      <div style={{width: '80%', margin: 'auto'}}>
+        <h1>This is a site to generate badass localstack sqs commands:</h1>
         <div className="container">
           <h3>What service are you working with?</h3>
           <div style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -83,7 +109,8 @@ function App() {
           <div className="container">
             <h3>Enter the js object you want to send:</h3>
             <textarea 
-              cols="50" rows="10"
+              style={{lineHeight: '25px', fontSize: '14px'}}
+              cols="100" rows="7"
               onChange={(e) => {setObject(JSON.parse(e.target.value))}} 
               value={JSON.stringify(object)}></textarea>
           </div>
@@ -91,7 +118,8 @@ function App() {
         }
         <div className="container">
           <h3>Here's your command, sir:</h3>
-          <div style={{padding: '5px', backgroundColor: 'lightgrey', textAlign: 'left'}}>{commandText}</div>
+          <div style={{padding: '5px', backgroundColor: 'lightgrey', textAlign:'left', lineHeight: '25px'}}>{commandText}</div>
+          <button id="copyButton" className="copyButton" onClick={() => {CopyCommand()}}>COPY</button>
         </div>
         <div style={{marginBottom: '100px'}} />
       </div>
